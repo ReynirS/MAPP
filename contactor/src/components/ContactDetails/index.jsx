bd13272df069eea60d2b6821ccb3c74aaa808ff9
Reynir Stefánsson <Reynir@Reynirs-MacBook-Pro-3.local>
2019-12-04 13:11:59 +0000

    styled contact details and added a non-functional call button

--- contactor/src/components/ContactDetails/index.jsx
+++ contactor/src/components/ContactDetails/index.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
-import {View, Text, Image } from 'react-native';
+import {View, Text, Image, Dimensions, TouchableOpacity } from 'react-native';
+import { Avatar } from 'react-native-elements';
+import styles from './styles';
+
+const {width} = Dimensions.get('window')
 
 const ContactDetails = ({contactName, contactNumber, contactImage}) => (
-  <View>
-    <Text>{contactName}</Text>
-    <Text>{contactNumber}</Text>
-    <Image
-      style={{height: 50, width: 50}}
+  <View style={styles.contact}>
+    <Avatar
+      containerStyle={styles.avatar}
+      size={width*0.8}
+      rounded
       source={{uri: contactImage}}
-      />
+      onPress={() => console.log("You just clicked the Avatar")}
+    />
+    <Text style={styles.text}>{contactName}</Text>
+    <Text style={styles.text}>{contactNumber}</Text>
+    <TouchableOpacity>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>Call me?</Text>
+      </View>
+    </TouchableOpacity>
   </View>
 );
 
---
+++ contactor/src/components/ContactDetails/styles.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create ({
+  contact: {
+    flex: 1,
+    alignItems: 'center'
+  },
+  avatar: {
+    marginTop: 50,
+  },
+  button: {
+    marginTop: 60,
+    padding: 20,
+    borderRadius: 10,
+    backgroundColor: 'white'
+  },
+  text: {
+    marginTop: 40,
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 20,
+  }
+});
