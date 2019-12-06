9d2ef19508a3dde7d8d787bc34a3af94992f4768
Reynir Stefánsson <Reynir@Reynirs-MacBook-Pro-3.local>
2019-12-06 18:24:53 +0000

    changed phonenumber input to phone-pad and added styling to nav bar

--- contactor/src/routes/index.js
+++ contactor/src/routes/index.js
@@ -4,9 +4,29 @@ import { createStackNavigator } from 'react-navigation-stack';
 import Main from '../views/Main';
 import Contact from '../views/Contact';
 
-const StackNavigator = createStackNavigator({
-  Main,
-  Contact,
-});
+const StackNavigator = createStackNavigator(
+  {
+    Main: {
+      screen: Main,
+      navigationOptions: {
+        title: 'Contacter',
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: '#615D6C'
+        }
+      }
+    },
+    Contact: {
+      screen: Contact,
+      navigationOptions: {
+        title: 'Details',
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: '#615D6C'
+        }
+      }
+    }
+  }
+);
 
 export default createAppContainer(StackNavigator);
