48a47d875eb6492253d5420419792d99e0790db2
Reynir Stefánsson <reynir17@ru.is>
2019-12-10 09:56:45 +0000

    changed the status bar to light-content

--- drCinema/src/views/Cinemas/index.jsx
+++ drCinema/src/views/Cinemas/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
 class Cinemas extends React.Component {
 
   render(){
     return(
       <View style={{ flex: 1} }>
+        <StatusBar barStyle='light-content' />
         <Text style={{ fontSize: 30 }}>Cinemas</Text>
       </View>
     );
--- drCinema/src/views/Upcoming/index.jsx
+++ drCinema/src/views/Upcoming/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StatusBar } from 'react-native';
 
 class Upcoming extends React.Component {
 
   render(){
     return(
       <View style={{ flex: 1 }}>
+        <StatusBar barStyle='light-content' />
         <Text style={{ fontSize: 30 }}>Upcoming</Text>
       </View>
     )
