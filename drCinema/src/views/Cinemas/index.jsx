0c99db8267eb3aa61e38f34088cff6ba300625d0
saemundur <saemundur17@ru.is>
2019-12-10 13:50:58 +0000

    get movie logic from api

--- drCinema/src/views/Cinemas/index.jsx
+++ drCinema/src/views/Cinemas/index.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import { View, Text, StatusBar } from 'react-native';
 
+const axios = require('axios');
+
+const gettingToken = async () => {
+  return axios.post('http://api.kvikmyndir.is/authenticate', {
+    username: 'danielmar18',
+    password: 'Skja9sia',
+  }).then(res => {
+    return res.data.token;
+  });
+}
+
+var gettingMovies = async () => {
+  const token = await gettingToken();
+  const theaters = await axios.get('http://api.kvikmyndir.is/theaters', {
+    headers: {
+      'x-access-token': token,
+    },
+  });
+  console.log(theaters.data.map(t => t.name));
+  //console.log(movies);
+  return theaters.data;
+}
+
+gettingMovies();
+
 class Cinemas extends React.Component {
 
   render(){
