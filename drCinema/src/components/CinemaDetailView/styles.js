01d32a26a270b99a58e2440ac92097ad4aed4534
Reynir Stefánsson <reynir17@ru.is>
2019-12-11 16:37:02 +0000

    website link is now active link to the actual website of the cinema in cinemaDetails

---
+++ drCinema/src/components/CinemaDetailView/styles.js
@@ -0,0 +1,31 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  name: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    margin: 10,
+  },
+  description: {
+    margin: 10,
+    marginTop: 0,
+    fontSize: 14,
+  },
+  address: {
+    margin: 10,
+    marginTop: 0,
+    fontSize: 14,
+  },
+  phoneNumber: {
+    margin: 10,
+    marginTop: 0,
+    fontSize: 14,
+  },
+  website: {
+    margin: 10,
+    marginTop: 0,
+    fontSize: 20,
+    color: 'blue',
+  },
+});
