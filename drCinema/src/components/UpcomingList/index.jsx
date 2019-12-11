1c7fc1dd142568eae92e9380aabaf4137538b433
Reynir Stefánsson <reynir17@ru.is>
2019-12-11 16:38:15 +0000

    just starting on upcoming movies

---
+++ drCinema/src/components/UpcomingList/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text, FlatList } from 'react-native';
+import { connect } from 'react-redux';
+// import UpcomingThumbnail from '../UpcomingThumbnail';
+// import styles from './styles';
+
+// const UpcomingList = props => {
+//
+// }
--- drCinema/src/views/Upcoming/index.jsx
+++ drCinema/src/views/Upcoming/index.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import { View, Text, StatusBar } from 'react-native';
+import { connect } from 'react-redux';
+import {getMoviesByCinemaID } from '../../actions/movieActions';
+import UpcomingList from '../../components/UpcomingList';
 
 class Upcoming extends React.Component {
 
+  getMoviesByCinemaID(){
+    const { getMoviesByCinemaID } = this.props;
+    getMoviesByCinemaID();
+  }
+
   render(){
+    this.getMoviesByCinemaID();
     return(
       <View style={{ flex: 1 }}>
         <StatusBar barStyle='light-content' />
-        <Text style={{ fontSize: 30 }}>Upcoming</Text>
+        <UpcomingList />
       </View>
     )
   }
 }
 
-export default Upcoming;
+export default connect(null, { getMoviesByCinemaID})(Upcoming);
